329cc86fcfcc956d6e9ee44fb35cf3c1aa80be38
arkie <email@joncaldwell.com>
2011-03-04 16:35:44 -0800

    abstract up parameterized managers

--- cereal/functions.py
+++ cereal/functions.py
@@ -1,28 +1,34 @@
 import cereal
 
-def filter(manager='_default_manager'):
-	"""
-	Usage:
-		cereal.register(Model, [filter()]) # Defaults to _default_manager
-		cereal.register(Model, [filter('objects')])
-	"""
-	def filter_fn(model, **kwargs):
-		"""
-		Applied filter on Model.%s
-		""" % manager
-		return cereal.ize(getattr(model, manager).filter(**kwargs))
-	filter_fn.__name__ = 'filter'
-	return filter_fn
+def queryset(queryset, **kwargs):
+	pass
 
-def page_filter(manager='_default_manager'):
+def managerize(function):
 	"""
-	Extends filter function to return pagination data.
+	Decorator to parameterize the manager in functions.
+	Usage:
+		cereal.register(Model, [function()]) # Defaults to _default_manager
+		cereal.register(Model, [function('objects')])
 	"""
-	def filter_fn(model, **kwargs):
-		"""
-		Applied paginated filter on Model.%s
-		""" % manager
-		ret = getattr(model, manager).filter(**kwargs)
-		return {'count':ret.count(), 'objects':cereal.ize(ret)}
-	filter_fn.__name__ = 'page_filter'
-	return filter_fn
+	fn_name = function.__name__
+	def manager_fn(manager='_default_manager'):
+		def inner_fn(model, **kwargs):
+			return function(model, manager, **kwargs)
+		inner_fn.__name__ = fn_name
+		return inner_fn
+	return manager_fn
+
+@managerize
+def filter(model, manager, **kwargs):
+	"""
+	Applied filter on Model.%s
+	""" % manager
+	return cereal.ize(getattr(model, manager).filter(**kwargs))
+
+@managerize
+def page_filter(model, **kwargs):
+	"""
+	Applied paginated filter on Model.%s
+	""" % manager
+	ret = getattr(model, manager).filter(**kwargs)
+	return {'count':ret.count(), 'objects':cereal.ize(ret)}
