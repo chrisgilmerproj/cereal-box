b45cce35159f3cde74c1e2d545fdcc03f508a1e3
Chris Gilmer <chris.gilmer@ff0000.com>
2011-03-09 10:00:52 -0800

    Updated the setup.py to fill it out.
    Added the license text.

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 from setuptools import setup
 
 setup(
-	name='cereal',
-	description='Data feed library for Django models.',
-	author='Red Interactive',
-	author_email='geeks@ff0000.com',
-	version='v0.1.0',
-	packages=['cereal'],
+	name = 'cereal',
+	packages = ['cereal',],
+	version = 'v0.1.0',
+	author = 'RED Interactive Agency',
+	author_email = 'geeks@ff0000.com',
+
+	url = 'http://www.github.com/ff0000/cereal-box/',
+	download_url = 'http://www.github.com/ff0000/cereal-box/',
+
+	license = 'MIT license',
+	description = 'Data feed library for Django models.',
+
+	long_description = open('README.md').read(),
+	requires = [],
+
+	classifiers = (
+		'Development Status :: 3 - Alpha',
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Natural Language :: English',
+		'Programming Language :: Python',
+		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+	),
 )
