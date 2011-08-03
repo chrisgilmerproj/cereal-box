79c1917344a5c0bae41e59107fd30d9e15d3d527
Chris Gilmer <chris.gilmer@gmail.com>
2011-08-02 17:47:46 -0700

    Cleaned up whitespace

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-	name = 'cereal-box',
-	packages = ['cereal',],
-	version = '1.0.3',
-	author = 'RED Interactive Agency',
-	author_email = 'geeks@ff0000.com',
+    name = 'cereal-box',
+    packages = ['cereal',],
+    version = '1.0.3',
+    author = 'RED Interactive Agency',
+    author_email = 'geeks@ff0000.com',
 
     package_data = {
         'cereal': [
@@ -13,23 +13,23 @@ setup(
         ]
     },
 
-	url = 'http://www.github.com/ff0000/cereal-box/',
-	download_url = 'http://www.github.com/ff0000/cereal-box/',
+    url = 'http://www.github.com/ff0000/cereal-box/',
+    download_url = 'http://www.github.com/ff0000/cereal-box/',
 
-	license = 'MIT license',
-	description = 'Data feed library for Django models.',
+    license = 'MIT license',
+    description = 'Data feed library for Django models.',
 
-	long_description = open('README.markdown').read(),
-	requires = [],
+    long_description = open('README.markdown').read(),
+    requires = [],
 
-	classifiers = (
-		'Development Status :: 3 - Alpha',
-		'Environment :: Web Environment',
-		'Framework :: Django',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Natural Language :: English',
-		'Programming Language :: Python',
-		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-	),
+    classifiers = (
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ),
 )
